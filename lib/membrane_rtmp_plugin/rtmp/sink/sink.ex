7aab5984ddfac4e04bcb448d8593327e5334b8ff
Przemyslaw Roznawski <roznawski.przemyslaw@gmail.com>
2022-10-18 17:15:46 +0200

    Add dts as fallback when no pts are present.

--- lib/membrane_rtmp_plugin/rtmp/sink/sink.ex
+++ lib/membrane_rtmp_plugin/rtmp/sink/sink.ex
@@ -211,7 +211,7 @@ defmodule Membrane.RTMP.Sink do
            state.native,
            buffer.payload,
            buffer.dts,
-           buffer.pts,
+           buffer.pts || buffer.dts,
            buffer.metadata.h264.key_frame?
          ) do
       {:ok, native} ->
